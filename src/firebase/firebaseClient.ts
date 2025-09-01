1e3f0fe1ec28e1c3d3779f71d1f3a9791e8fa5b6
Samasoft <sama.s.soft@gmail.com>
2025-09-01 19:41:24 +0300

    تحديث إعدادات Firebase في firebaseClient.ts لاستبدال المتغيرات البيئية بقيم ثابتة، مما يسهل الاتصال بخدمات Firebase. هذا التغيير يعزز من وضوح الإعدادات ويضمن تكامل المشروع مع بيئة الإنتاج.

--- src/firebase/firebaseClient.ts
+++ src/firebase/firebaseClient.ts
@@ -5,12 +5,12 @@ import { getMessaging, isSupported } from 'firebase/messaging';
 import { getStorage, connectStorageEmulator } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBgobPK7uwaAcGEUxBpi7WY7sAIDlIt5sQ",
+  authDomain: "mayor-plan.firebaseapp.com",
+  projectId: "mayor-plan",
+  storageBucket: "mayor-plan.appspot.com",
+  messagingSenderId: "604154242666",
+  appId: "1:604154242666:web:a109449fae6dd1bd908c13",
 };
 
 // تهيئة Firebase
